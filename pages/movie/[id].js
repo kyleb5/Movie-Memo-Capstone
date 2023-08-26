860448fc6590c326855eb64953618beae5fdfeae
Kyle B <kberry1298@gmail.com>
2023-08-26 09:31:09 -0500

    made details look resonable for now

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { Card } from 'react-bootstrap';
 import { getMovieById } from '../../utils/data/themoviedb';
 
 export default function ViewMovie() {
-  const [movieDetails, setMovieDetails] = useState([]);
+  const imagePath = 'https://image.tmdb.org/t/p/original';
+  const [movieDetails, setMovieDetails] = useState({});
   const router = useRouter();
   const { id } = router.query;
 
   useEffect(() => {
-    getMovieById(id).then(setMovieDetails);
+    if (id) {
+      getMovieById(id).then(setMovieDetails);
+    }
   }, [id]);
 
   return (
-    <div className="pushdown-top">
-      {movieDetails.title}
-      <p>{movieDetails.overview}</p>
+    <div className="image-container">
+      <Card.Img className="image-style" src={`${imagePath}${movieDetails.backdrop_path}`} />
+      <div>
+        <h1 style={{ fontSize: '1.5rem', marginBottom: '0.5rem' }}>{movieDetails.title}</h1>
+        <p style={{ fontSize: '1rem' }}>{movieDetails.overview}</p>
+      </div>
     </div>
   );
 }
